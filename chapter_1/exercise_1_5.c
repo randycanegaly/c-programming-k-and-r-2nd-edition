c8d9d732eb20529c7d53db345f06352005057813
randycanegaly <canegaly@gmail.com>
2024-08-14 08:24:59 -0400

    Completed exercise 1.5

---
+++ chapter_1/exercise_1_5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+//using symbolic constants
+#define LOWER 	 0
+#define UPPER  300
+#define STEP 	20
+
+int main() {
+	int fahr;
+
+	printf("Fahr    Celsius\n");
+	
+	for(fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP) {
+		printf("%3d    %6.1f\n", fahr, (5.0/9.0)*(fahr-32));//calculate celsius inline
+	}	
+}
