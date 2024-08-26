6bfe58bdb6b2773d713f3ba52954e7635cf849ae
randycanegaly <canegaly@gmail.com>
2024-08-25 23:33:30 -0400

    Completed exercise 1.17.

--- chapter_1/ex_1_17.c
+++ chapter_1/ex_1_17.c
@@ -1,26 +1,17 @@
 #include <stdio.h>
 #define MAXLINE 1000 //max input line length
-#define LONGLINE 80
 
 int mygetline(char line[], int maxline);
-void copy(char to[], char from[]);
 
-/* print all input lines longer than 80 characters*/
+/* print all input lines longer than 80 characters */
 int main() {
-	int len;
-	int max;
+	int len;				//length of current line
 	char line[MAXLINE];
-	char longest[MAXLINE]; 
-
-	max = 0; 			
-	while ((len = mygetline(line, MAXLINE)) > 0) 
-		if(len > LONGLINE) {
-			max = len;
-			copy(longest, line);
-		}
-	if (max > 0) {	
-		printf("longest: %s\n", longest);
+	while ((len = mygetline(line, MAXLINE)) > 0) {
+		if (len > 80)
+			printf("%s", line);	
 	}
+	
 	return 0;
 }
 
@@ -28,26 +19,18 @@ int main() {
 int mygetline(char s[], int lim) {
 	int c, i;
 
-	for (i=0; i<lim-1 && (c = getchar()) != EOF && c!='\n'; ++i) 
-	//stop one shy of max because we have to stick in '0' at the end of the array
-	//stop if the character is end of file
-	//don't process anything if it's the newline character
-		s[i] = c; //stick character in s if not near end, not EOF and not newline
-	
-	if (c == '\n') {//special case, do the thing with newline that the for loop ignored
-		s[i] = c;//stick newline in the array
+	for (i=0; i<lim-1 && (c = getchar()) != EOF && c!='\n'; ++i) {
+		//printf("i: %d, c: %c\n", i, c);
+		s[i] = c; 
+	}
+
+	if (c == '\n') {
+		//printf("out of for, i: %d, c: %c\n", i, c);
+		s[i] = c;
 		++i; 
 	}
-	s[i] = '0';//always stick '0' to mark end of character array
-	return i; //return the length found
+	s[i] = '\0';
+	return i; 
 }
 
-/*  copy from into to  */
-void copy(char to[], char from[]) {
-	int i;
-	
-	i = 0;
-	while((to[i] = from[i]) != '0') {
-		++i;
-	}
-}
+
