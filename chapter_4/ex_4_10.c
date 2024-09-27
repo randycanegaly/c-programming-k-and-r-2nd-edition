52965aaa19feed148af3ed2da0219aa293690d18
randycanegaly <canegaly@gmail.com>
2024-09-27 12:32:03 -0400

    Working on exercise 10 in chapter 4.

---
+++ chapter_4/ex_4_10.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h> /*for atof */
+
+#define MAXOP 100 /*max size for operand or operator */
+#define NUMBER '0' /*signal that a number was found */
+
+int getop(char []);
+void push(double);
+double pop(void);
+
+/* reverse polish calculator */
+/* use getline to read an entire line. Modify to not use getch or ungetch */:w
+
+int main(void) {
+    int type;
+    double op2;
+    char s[MAXOP];
+
+    while((type = getop(s)) != EOF) {
+        switch(type) {
+        case NUMBER:
+            push(atof(s));
+            break;
+        case '+':
+            push(pop() + pop());
+            break;
+        case '*':
+            push(pop() * pop());
+            break;
+        case '-':
+            op2 = pop();
+            push(pop() - op2);
+            break;
+        case '/':
+            op2 = pop();
+            if (op2 != 0.0)
+                push(pop() / op2);
+            else
+                printf("error: dividing by zero");
+            break;
+        case '\n':
+            printf("\t%.8g\n", pop());
+            break;
+        default:
+            printf("error: bad command%s\n", s);
+            break;
+        }
+    }
+    return 0;
+}
+
+#define MAXVAL 100 /*maximum depth of val stack */
+
+int sp = 0; /*next free stack position */
+double val[MAXVAL]; /*value stack*/
+
+/* push: push f onto the stack */
+void push(double f) {
+    if (sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("error: stack full, can't push %g\n", f);
+}
+
+/* pop: pop and return value at the top of the stack */
+double pop(void) {
+    if (sp > 0)
+        return val[--sp];
+    else {
+        printf("error: stack empty\n");
+        return 0.0;
+    }
+}
+
+#include <ctype.h>
+
+int getch(void);
+void ungetch(int);
+int mygetline(char s[], int lim);
+
+/*getop: get next operator or numeric operand */
+int getop(char s[]) {//going to be pushing characters into s as we parse them
+    //get an entire line and then process it for operators and operands */
+
+    char line[MAXOP];
+
+    mygetline(line, MAXOP);
+    int i, c;
+
+    //line now has the whole math equation like "3 4 +"
+    //walk that line and look for:
+    //  white space
+    //  operators
+    //  left digits
+    //  decimal places
+    //  right digits
+    while((s[0] = c = getch()) == ' ' || c == '\t')
+        ;
+    s[1] = '\0';//TO DO - what?
+    if(!isdigit(c) && c != '.')
+        return c;//return an operator
+    i = 0;
+    if (isdigit(c)) /*gather up an integer part */
+        while (isdigit(s[++i] = c = getch()))
+                ;
+    if (c == '.')
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    s[i] = '\0';
+    if (c != EOF)
+        ungetch(c);
+    return NUMBER;
+}
+
+/* read an input line into s, return its length */
+int mygetline(char s[], int lim) {
+    int c, i;
+
+    for (i=0; i<lim-1 && (c = getchar()) != EOF && c!='\n'; ++i) {
+        s[i] = c;
+    }
+
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i-1;
+}
+
+/*
+#define BUFSIZE 100
+char buf[BUFSIZE];
+int bufp = 0; 
+
+int getch(void) {
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) {
+    if (bufp  >= BUFSIZE)
+        printf("ungetch: too many characters");
+    else 
+        buf[bufp++] = c;
+}
+*/
