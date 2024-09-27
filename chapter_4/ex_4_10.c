1fb349bac91f71f0d0b32d57f66647e3182fcfb7
randycanegaly <canegaly@gmail.com>
2024-09-27 13:02:27 -0400

    Working on getop() in exercise 10, chapter 4.

--- chapter_4/ex_4_10.c
+++ chapter_4/ex_4_10.c
@@ -78,13 +78,16 @@ int getch(void);
 void ungetch(int);
 int mygetline(char s[], int lim);
 
+//getop gets called multiple times when processing an input
+//so need a walker variable external to getop to track where we are in the input line
+int lw;
+
 /*getop: get next operator or numeric operand */
-int getop(char s[]) {//going to be pushing characters into s as we parse them
-    //get an entire line and then process it for operators and operands */
-
+int getop(char s[]) {//when something (number or operator or newline) is found put it in s for the caller to use
+    //get an entire line and then process it for operators and operands 
     char line[MAXOP];
-
     mygetline(line, MAXOP);
+    
     int i, c;
 
     //line now has the whole math equation like "3 4 +"
@@ -94,19 +97,25 @@ int getop(char s[]) {//going to be pushing characters into s as we parse them
     //  left digits
     //  decimal places
     //  right digits
-    while((s[0] = c = getch()) == ' ' || c == '\t')
+    
+    //process line for whitespace
+    while((c = line[lw++]) = ' ' || c == '\t') 
         ;
-    s[1] = '\0';//TO DO - what?
+
     if(!isdigit(c) && c != '.')
-        return c;//return an operator
-    i = 0;
+        return c;//send an operator signal to the caller
+    lw++;
+    
+    i = 0; 
     if (isdigit(c)) /*gather up an integer part */
         while (isdigit(s[++i] = c = getch()))
                 ;
+    
     if (c == '.')
         while (isdigit(s[++i] = c = getch()))
             ;
     s[i] = '\0';
+    
     if (c != EOF)
         ungetch(c);
     return NUMBER;
