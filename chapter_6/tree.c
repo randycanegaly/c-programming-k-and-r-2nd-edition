adbda3a1d0b9b04db52a47ac68d9f2aab2ec1d7f
randycanegaly <canegaly@gmail.com>
2024-10-21 22:20:40 -0400

    starting work on ex 2 in chap 6. Just added some design approach comments.

---
+++ chapter_6/tree.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAXWORD 100
+struct tnode *addtree(struct tnode *, char *);
+void treeprint(struct tnode *);
+int getword(char *, int);
+
+struct tnode {
+    char *word;
+    int count;
+    struct tnode *left;
+    struct tnode *right;
+};
+
+/* word frequency count */
+int main(void) {
+    struct tnode *root;
+    char word[MAXWORD];
+
+    root = NULL;
+    while (getword(word, MAXWORD) != EOF)
+        if (isalpha(word[0]))
+            root = addtree(root, word);
+    treeprint(root);
+    return 0;
+}
+
+struct tnode *talloc(void);
+char *mystrdup(char *);
+
+/* addtree: add a node with w, at or below p */
+struct tnode *addtree(struct tnode *p, char *w) {
+    int cond;
+
+    if (p == NULL) { //base case. this is a new word
+        p = talloc(); //create a new node
+        p->word = mystrdup(w);
+        p->count = 1;
+        p->left = p -> right = NULL;
+    } else if ((cond = strcmp(w, p->word)) ==0)
+        p->count++;
+    else if (cond < 0)
+        p->left = addtree(p->left, w);
+    else
+        p->right = addtree(p->right, w);
+    return p;
+}
+
+/* getword: get next word or character from input */
+int getword(char *word, int lim) {
+    int c, getch(void);
+    void ungetch(int);//going to use these two functions in here, have to declare them first
+    char *w = word;
+
+    while (isspace(c = getch())) //skip white space
+        ;
+    if (c != EOF) //not a space and not end of file
+        *w++ = c; //* and ++ have equal precedence and associate right to left. increment w pointer to next character, assign c to what the pointer points to
+    if (!isalpha(c)) { //if c is not an alpha, terminate the w string, return that character
+        *w = '\0';
+    return c;
+    }
+    for ( ; --lim > 0; w++) //walk the word up to the limit
+        if (!isalnum(*w = getch())) { //get the next character, if it's not an alpha or number, push that too far character back
+            ungetch(*w);
+            break;
+        }
+    *w = '\0'; //past the end of the word, terminate the string
+    return word[0];
+}
+
+/* treeprint: in-order print of tree p */
+void treeprint(struct tnode *p)
+{
+    if (p != NULL) {
+        treeprint(p->left);
+        printf("%4d %s\n", p->count, p->word);
+        treeprint(p->right);
+    }
+}
+
+#include <stdlib.h>
+
+/* talloc: make a tnode */
+struct tnode *talloc(void)
+{
+    return (struct tnode *) malloc(sizeof(struct tnode));
+}
+
+char *mystrdup(char *s)
+{
+    char *p;
+
+    p = (char *) malloc(strlen(s) + 1); /* +1 for the ending '\0' */
+    if (p != NULL);
+        strcpy(p, s);
+    return p;
+}
+
+#define BUFSIZE 100
+char buf[BUFSIZE];
+int bufp = 0;
+int getch(void) {
+    return (bufp > 0) ? buf[--bufp] : getchar();
+} 
+
+void ungetch(int c) {
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
+}
