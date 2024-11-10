82ab4d0397a9ad2f6fcbe6786dbf3ff8bb512808
randycanegaly <canegaly@gmail.com>
2024-11-10 18:57:23 -0500

    entered mintprintf from book, process showing killed. On MacOS, not usual practice. Commit here and try to debug in Linux.

---
+++ chapter_7/minprintf.c
@@ -0,0 +1,55 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+void minprintf(char *, ...);
+
+int main(void) {
+	minprintf("%d", 101);		
+	minprintf("%f", 101.01);		
+	//minprintf("%s", "hoochie");		
+}
+
+/* minprintf: minimal printf with variable argument list */
+void minprintf(char *fmt, ...) {
+	va_list ap; /* points to each unamed arg in turn */
+	char *p, *sval; 
+	int ival;
+	double dval;
+
+	va_start(ap, fmt); /* make ap point to the 1st unnamed arg. va_start
+						  is a macro that enables the va_list so that it can
+						  be used to access the variable argument list.
+						  the second argument to va_start is the last parameter in the 
+						  function signature before the ... of variable argument list */
+	for (p = fmt; *p; p++) { //init pointer p to fmt, as long as pointer p is not
+							 //null, increment pointer - do pointer math
+		if (*p != '%') {//if the thing p points to is not %, emit the character
+			putchar(*p);
+			continue;	//go immediately and do the next iteration of the loop
+		} 
+		switch (*++p) {//if we got to here, then we just saw a %
+					   //same operator precedence, right to left, so dereference
+					   //p and then increment - means move to the next character
+					   //and use that character as input for the switch
+		case 'd'://see %d
+			ival = va_arg(ap, int);//va_arg retrieves the next argument from
+								   //the variable argument list
+								   //its second argument is the type to be 
+								   //retrieved
+			printf("%d", ival);
+			break;
+		case 'f':
+			dval = va_arg(ap, double);
+			printf ("%f", dval);
+			break;
+		case 's':
+			for (sval = va_arg(ap, char *); *sval; sval++)
+				putchar(*sval);
+			break;	
+		default:
+			putchar(*p);
+			break;
+		}
+	}
+	va_end(ap);
+}
